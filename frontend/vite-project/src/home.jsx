e9e64a43f83faa72cc27ebb55c4d57f567439a98
balaji <balajier2006@gmail.com>
2025-03-06 13:44:37 +0530

    done

---
+++ frontend/vite-project/src/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div>
+        <h4>Hello Everyone ✌️</h4>
+        <h4>
+            on construction
+        </h4>
+        </div>
+    )
+}
+export default Home;
